cde82467c4a06531b56d86a3d7b9995f333f072f
Lorenzo Monacelli <mesonepigreco@gmail.com>
2022-07-08 17:06:21 +0200

    Fixed the status of the enemies

--- js/enemies.js
+++ js/enemies.js
@@ -13,8 +13,36 @@ export class Wolf extends Character {
         this.attack_trigger = -1000;
         this.attack_range = 10;
 
+        this.spot_trigger = -1000;
+        this.spot_countdown = 200;
+
         this.target = null;
         this.raycast = raycast;
+        this.facing_direction = "down";
+        this.angle_start = 0;
+        this.angle_end = 0;
+        this.spotted = false;
+    }
+
+    get_angle() {
+        let angle_start = 0;
+        let angle_end = 0;
+
+        if (this.facing_direction === "down") {
+            angle_start = -2.3561;
+            angle_end = -0.7854;
+        } else if (this.facing_direction === "right") {
+            angle_start = -0.7854;
+            angle_end = 0.7854;
+        } else if (this.facing_direction === "up") {
+            angle_start = 0.7854;
+            angle_end = 2.3561;
+        } else if (this.facing_direction === "left") {
+            angle_start = 2.3561;
+            angle_end = 3.9270;
+        }
+        this.angle_start = angle_start;
+        this.angle_end = angle_end;
     }
 
     move_to(position) {
@@ -29,7 +57,7 @@ export class Wolf extends Character {
 
     attack(enemy) {
         let time = Date.now();
-        if (distance(this, enemy) < this.attack_range) {
+        if (distance(this.center, enemy.center) < this.attack_range) {
             if (time - this.attack_trigger > 1000 / this.attack_rate) {
                 // TODO: Show the animation of the attack
 
@@ -44,7 +72,33 @@ export class Wolf extends Character {
         }
     }
 
-    update(dt) {
-        
+    update(dt, player = null) {
+        let time = Date.now();
+        if (player !== null) {
+            // Check if the enemy can see the player.
+            this.get_angle();
+            let visible = this.raycast.project_ray(this.center, player.center, this.fov, this.angle_start, this.angle_end);
+            
+            if (visible && !this.spotted) {
+                // Check if we need to play an animation or a spotted sound
+                if (time - this.spot_trigger > this.spot_countdown) {
+                    // Spotted for the first time
+                    this.spotted = true;
+                    this.spot_trigger = time;
+                    
+                    // TODO: Play animation and sound
+                }
+            }
+
+            if (visible) {
+                // Update the target
+                this.move_to(player.center);
+                this.attack(player);
+            }
+        }
+
+        super.update(dt);
+
+        // TODO: update the status
     }
 }
